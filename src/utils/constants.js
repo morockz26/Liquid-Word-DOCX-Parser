30ff55dbc0474e0914796efc63882dc93f1cbb22
Mohammed Osumah <mohammedo@wingmateapp.com>
2023-12-31 10:16:31 -0500

    removed parser from js from git ignore

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
 module.exports = {
-  TABLE_ROW_REGEX: /<w:tr\b[^>]*>([\s\S]*?)<\/w:tr>/gm,
-  FOR_LOOP_REGEX: /{%\s*for\s+\w+(\.\w+)?\s+in\s+\w+(\.\w+)?\s*%}/,
+  BROKEN_TAG_REGEX:
+    /<w:p[^>]*>[\s\S]*?((?:{{|{%)[\s\S]*?(?:}}|%})[\s\S]*?(?=<\/w:t>))[\s\S]*?<\/w:p>/g,
   END_FOR_REGEX: /{%\s*endfor\s*%}(?![\s\S.]*{%\s*endfor\s*%})/,
+  FOR_LOOP_REGEX: /{%\s*for\s+\w+(\.\w+)?\s+in\s+\w+(\.\w+)?\s*%}/,
+  TABLE_ROW_REGEX: /<w:tr\b[^>]*>([\s\S]*?)<\/w:tr>/gm,
   LIQUID_REGEX_PATTERN:
     /(?:\{%\s*(?:(?:for|if)\s+[^{}]*\s*%})[\s\S]*?\{%\s*end(?:for|if)\s*%})|(?:\{%\s*assign\s+[^{}]*\s*%\})|(?:\{\{[^{}]*\}\})/gm,
-  LIQUID_TAG_REGEX: /{.*}/gm,
+  LIQUID_TAG_SPLIT_REGEX: /<\/w:t>[\s\S]*?<(?:w:t)[^>^(?:tab)]*>/g,
   specialCharacterMap: {
     "&lt;": "<",
     "&gt;": ">",
